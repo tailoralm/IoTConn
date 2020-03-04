e2c3ad76c511d1f88e9160389b6ad8c282048411
Tailor <tailor.alm@gmail.com>
2020-03-04 19:06:24 -0300

    #001 - app com firebase

---
+++ app/src/main/java/com/example/iotconn/activitys/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.iotconn.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.iotconn.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
